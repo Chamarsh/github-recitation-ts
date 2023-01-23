c4c0182a0db351ebd7998a0c4f8997b74db61f48
Chamarsh <tdchase34@gmail.com>
2023-01-23 09:39:22 -0500

    Removed type for string literal inference

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -14,7 +14,7 @@ export default (req: Req, res: Res) => {
   const num: string = req.params;
 
   const fibN: number = fibonacci(parseInt(num));
-  let result: string = `fibonacci(${num}) is ${fibN}`;
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
